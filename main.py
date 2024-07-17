3e78fb79bee32a8ef76602493ec697e8b371fb68
Joel Fergusson <joel@fergfamily.co.uk>
2024-07-17 16:48:07 +0100

    Added basic admin page

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def line_span(a, b):
 class AbstractSlide:
     def get_text(self):
         return NotImplementedError
-    
+
 class ScoreSlide(AbstractSlide):
     def __init__(self):
         # Read scores from file
@@ -32,14 +32,21 @@ class ScoreSlide(AbstractSlide):
                 except Exception:
                     pass
 
-        self.scores = [[i, scores_dict[i]] for i in sorted(scores_dict)]
+        self.scores = [i for i in sorted(scores_dict.items(), key=lambda item: item[1])]
 
     def get_text(self):
         lines = ["Dorm points", "", line_span("Dorm", "Score"),]
         for score in self.scores:
             lines.append(line_span(score[0], str(score[1])))
         return "\n".join(lines)
-    
+
+    def get_html(self):
+        text = "<table>"
+        for score in self.scores:
+            text += "<tr><td>{}</td><td>{}</td></tr>".format(*score)
+        text += "</table>"
+        return text
+
 
 class TimetableSlide(AbstractSlide):
     def __init__(self):
@@ -58,7 +65,7 @@ class TimetableSlide(AbstractSlide):
                 day = [i for i in day if "Y" in i[1]]
                 days.append(day)
                 day = []
-        
+
         day = [i for i in day if "Y" in i[1]]
         days.append(day)
         days = days[1:]
@@ -87,10 +94,10 @@ class TimetableSlide(AbstractSlide):
             if event[0] > datetime.datetime.now():
                 if event[0].date() == datetime.datetime.now().date():
                     next_events.append(event)
-        
+
         if len(next_events) > 5:
             next_events = next_events[:5]
-        
+
         lines = []
         lines.append("Timetable")
         lines.append("")
@@ -98,14 +105,14 @@ class TimetableSlide(AbstractSlide):
             lines.append("{} - {}".format(e[0].time().strftime("%H:%M"), e[1]))
 
         return "\n".join(lines)
-    
+
 class TextSlide(AbstractSlide):
     def __init__(self, text):
         self.text = text
 
     def get_text(self):
         return self.text
-        
+
 
 class MyHandler(BaseHTTPRequestHandler):
     def __init__(self, server, *args, **kwargs):
@@ -121,20 +128,41 @@ class MyHandler(BaseHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(data)
 
+    def do_POST(self):
+        url = urlparser.urlparse(self.path)
+        content_len = int(self.headers.get('content-length', 0))
+        post_body = self.rfile.read(content_len)
+        response, content_type, data = self._server.serve(url.path, post_body)
+        self.send_response(response)
+        self.send_header("Content-type", content_type)
+        self.end_headers()
+        self.wfile.write(data)
+
+
 class Server:
     def __init__(self):
         self._urls = [
             (r'^/display/?$', self.show_display_page),
             (r'^/admin/?$', self.show_admin_page),
             (r'^/update/?$', self.get_data_update),
+            (r'^/score$', self.update_score),
         ]
-        self._slides = [
-            TextSlide("Welcome to LiveWires 2024"),
-            TimetableSlide(),
-            ScoreSlide(),
-        ]
+        self._timetable_slide = None
         self.last_update = datetime.datetime.now()
         self.slide_index = 0
+        self._reload_slides()
+
+    def _reload_slides(self):
+        self._score_slide = ScoreSlide()
+        if self._timetable_slide is None:
+            self._timetable_slide = TimetableSlide()
+
+        self._slides = [
+            self._score_slide,
+            TextSlide("Welcome to LiveWires 2024"),
+            self._timetable_slide,
+        ]
+        self.slide_index = min(self.slide_index, len(self._slides))
 
     def serve_html_file(self, fname):
         with open(fname, "rb") as f:
@@ -148,9 +176,9 @@ class Server:
 
     def show_display_page(self, params):
         return self.serve_html_file("display.html")
-    
+
     def show_admin_page(self, params):
-        return 200, "text/html", b"<html><h2>Admin</h2></html>"
+        return self.serve_html_file("admin.html")
 
     def get_data_update(self, params):
         now = datetime.datetime.now()
@@ -160,14 +188,24 @@ class Server:
         slide = self._slides[self.slide_index]
         return 200, "application/json", bytes(json.dumps({"new_text": slide.get_text()}), "utf-8")
 
+    def update_score(self, data):
+        params = json.loads(data.decode("utf-8"))
+        word = "added to" if int(params["points"]) >= 0 else "subtracted from"
+        if int(params["points"]) != 0:
+            with open("scores.csv", "a") as f:
+                f.write("\n{},{}".format(params["dorm"], params["points"]))
+        self._reload_slides()
+        feedback = "{} points {} dorm {}<br><br>{}".format(params["points"], word, params["dorm"], self._score_slide.get_html())
+
+        return 200, "application/json", bytes(json.dumps({"feedback": feedback}), "utf-8")
+
 if __name__ == '__main__':
     server = Server()
     handler = partial(MyHandler, server)
     httpd = HTTPServer((HOST_NAME, PORT_NUMBER), handler)
-    
+
     try:
         httpd.serve_forever()
     except KeyboardInterrupt:
         pass
     httpd.server_close()
-    
